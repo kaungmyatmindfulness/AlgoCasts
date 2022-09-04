7f0002ebc9ed7e9b5c602e2b687451153d1306ab
KaungMyat <softcraftkmh@gmail.com>
2022-09-05 06:17:15 +0630

    feat: anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,29 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+const buildCharMap = (str) => {
+	const charMap = {};
+	for (const char of str.replace(/[^\w]/g, "").toLowerCase()) {
+		charMap[char] = charMap[char] + 1 || 1;
+	}
+	return charMap;
+};
+
+function anagrams(stringA, stringB) {
+	const aCharMap = buildCharMap(stringA);
+	const bCharMap = buildCharMap(stringB);
+
+	if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+		return false;
+	}
+
+	for (const char in aCharMap) {
+		if (aCharMap[char] !== bCharMap[char]) {
+			return false;
+		}
+	}
+
+	return true;
+}
 
 module.exports = anagrams;
