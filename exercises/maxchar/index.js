910318275efd12548af3a55e5426a11c6920f323
KaungMyat <softcraftkmh@gmail.com>
2022-09-05 05:26:54 +0630

    feat: maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,23 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+	const charMap = {};
+	let max = 0;
+	let maxChar = "";
+
+	[...str].forEach((e) => {
+		charMap[e] = charMap[e] + 1 || 1;
+	});
+
+	for (const char in charMap) {
+		if (charMap[char] > max) {
+			max = charMap[char];
+			maxChar = char;
+		}
+	}
+
+	return maxChar;
+}
 
 module.exports = maxChar;
