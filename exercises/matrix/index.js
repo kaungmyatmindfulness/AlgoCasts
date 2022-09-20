565ae48530c692fc08ebb27cc572893974e3b939
KaungMyat <softcraftkmh@gmail.com>
2022-09-20 14:33:10 +0630

    wip: spiral

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,40 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+/**
+ *
+ * 0	1	2	3
+ * 4	5	6	7
+ * 8	9	10	11
+ * 12	13	14	15
+ *
+ * 4 3 3 2 2 1 1 1
+ */
+
+/**
+ *
+ * 0	1	2	3	4
+ * 5	6	7	8	9
+ * 10	11	12	13	14
+ * 15	16	17	18	19
+ * 20	21	22	23	24
+ *
+ *
+ * 5 4 4 3 3 2 2 1 1 1
+ */
+
+const insertRight = (index, value, n) => {};
+
+const insertBottom = () => {};
+
+const insertLeft = () => {};
+
+const insertTop = () => {};
+
+function matrix(n) {
+	const numOfElements = n * n;
+
+	[...Array(n)].forEach((e, i) => {});
+}
 
 module.exports = matrix;
